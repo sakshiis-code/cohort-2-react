89b9242e7d27d1bd9db8f2e91757e00aa7dc5891
sakshiis-code <sakshihihisharma@gmail.com>
2025-12-17 21:18:57 +0530

    Initial commit - React project

---
+++ Day 66 Introduction to React/app.js
@@ -0,0 +1,3 @@
+const user = 'Sarthak'
+
+export default user
---
+++ Day 66 Introduction to React/script.js
@@ -0,0 +1,21 @@
+var arr = [10, 20, 30, 40]
+// var arr2 = arr
+
+// arr2.push(99)
+// console.log(arr);
+
+// var arr2 = [arr[0], arr[1], arr[2], ...arr[n]]
+
+var arr2 = [...arr]
+
+
+
+// import export
+// code separated
+
+// default export
+// default import
+
+import user from "./app.js";
+
+console.log(user);
